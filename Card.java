5c1b434b939e7f8ded85dc2b4d484dcacfa72bb2
batthkan <123487732+batthkan@users.noreply.github.com>
2023-10-04 23:51:00 -0400

    Update Card.java

--- Card.java
+++ Card.java
@@ -1,25 +1,6 @@
 package cardgame;
 
-/**
- * Starter code for the Card class.
- * To be used in Week 4.
- * @author dancye, 2019
- */
 
-/*
-    Explaination why the given java code can be used and extend to the UNO game:-
-
-    Inheritance is the major responsible that is responsible for the reusability and extension of the code. It means that a code or variables created in a classs can be further used in other classes.
-*/
-
-/*
-    Eplain the feauture responsible:-
-
-    The Card class may represent a single card independently because of its modular structure.
-    The class's uncomplicated attributes and methods are clearly stated and may be changed or expanded to suit the requirements of another programme. Its adaptability makes it simple to reuse or expand the Card class in other applications that call for a typical deck of cards.
-    By creating numerous Card objects and including them in a deck, the class, for instance, may be utilised in other games or software.
-    MOreover, cohesion plays an important role in this feature.
-*/
 public class Card {
     
         public enum Color{RED, YELLOW, GREEN, BLUE};
